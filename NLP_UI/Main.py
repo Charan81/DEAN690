01cc08e1e47ea7caed1d8e46880cc4fc0fbe64b3
Navy Merianda <NV@Navys-MacBook-Pro.local>
2020-11-20 22:38:15 -0500

    Fixed minor bugs.

--- NLP_UI/Main.py
+++ NLP_UI/Main.py
@@ -20,30 +20,23 @@ def query():
     if request.method=='POST':
        
         name = request.form['name']
-        #fieldnames = ['name']
-        time.sleep(1)
 
-        result_2 = processquery.Process(name) # Run temp.py 
+        resultSet = processquery.Process(name) 
 
-        if result_2 is None:
-            return render_template("error.html", variable = result_2,  data="Please check if your query is compatible with our system!")
+        if resultSet is None:
+            return render_template("error.html", variable = resultSet,  data="Please check if your query is compatible with our system!")
         
-        print(type(result_2))
-
-        print(result_2)
-        
-        numberOfColumns = len(result_2.columns)
+        numberOfColumns = len(resultSet.columns)
         if numberOfColumns == 1:
-            if result_2[' '].iloc[0] is not None:
-                splitValue = str(round(result_2[' '].iloc[0], 2))
+            if resultSet[' '].iloc[0] is not None:
+                splitValue = str(round(resultSet[' '].iloc[0], 2))
             else:
                 splitValue = 'None'
-            print(splitValue)
-            return render_template("singlevaluetemplate.html", variable = result_2,  data=splitValue, query = name)
+            return render_template("singlevaluetemplate.html", variable = resultSet,  data=splitValue, query = name)
         else:
-            return render_template("tabletemplate.html", variable = result_2,  data=result_2.to_html(classes=["table-sm"],header=True) , query = name)
+            return render_template("tabletemplate.html", variable = resultSet,  data=resultSet.to_html(classes=["table-sm"],header=True) , query = name)
 
-        #return None
+        return None
 
         
 """ if __name__ == "__main__":
